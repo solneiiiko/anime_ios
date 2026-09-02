0b71d0306e1d8cb2aad075400f56db57c667a6d3
solneiiiko <solneiiiko@gmail.com>
2026-09-02 19:50:08 +1000

    Initial Commit

---
+++ anime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  anime
+//
+//  Created by Oks on 2/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ anime/animeApp.swift
@@ -0,0 +1,17 @@
+//
+//  animeApp.swift
+//  anime
+//
+//  Created by Oks on 2/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct animeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
