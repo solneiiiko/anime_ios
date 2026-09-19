7bf8612f58a5a10f09b33a2872fb1ab641166482
solneiiiko <solneiiiko@gmail.com>
2026-09-20 00:02:10 +1000

    DesignSystem with colours

---
+++ anime/Modules/DesignSystem/Sources/DesignSystem/Theme/AppColors.swift
@@ -0,0 +1,21 @@
+//
+//  AppColors.swift
+//  DesignSystem
+//
+//  Created by Oks on 19/9/2026.
+//
+
+import SwiftUI
+
+public enum AppColors {
+    public static let accent = Color("AccentColor", bundle: .module)
+    public static let textPrimary = Color("TextPrimary", bundle: .module)
+    public static let textSecondary = Color("TextSecondary", bundle: .module)
+    public static let textOnAccent = Color("TextOnAccent", bundle: .module)
+    public static let textOnDestructive = Color("TextOnDestructive", bundle: .module)
+    public static let backgroundPrimary = Color("BackgroundPrimary", bundle: .module)
+    public static let backgroundSecondary = Color("BackgroundSecondary", bundle: .module)
+    public static let backgroundTertiary = Color("BackgroundTertiary", bundle: .module)
+    public static let separator = Color("Separator", bundle: .module)
+    public static let destructive = Color("Destructive", bundle: .module)
+}
